3254a7c4c03c733278ff376f5c57c2e15b418e18
chiakiiwasaki <chiaki103ki@gmail.com>
2021-06-05 12:29:57 +0000

    [Add]4-0

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f920adcaf78d9bf7cd7003fb61344fc902d7ba13d8c2faba3c19b95e94b841362534e94aea30f7dd2b9ee04096cb02f84a015581c74ab08b0629c0a13ad6165d'
